28dea88a30b795e42005fd326c70c227d75c3970
DEPANJAN88 <depanjan.nath@gmail.com>
2025-04-22 22:50:24 +0530

    Add files via upload

---
+++ grades of students.py
@@ -0,0 +1,11 @@
+Marks=int(input("Enter the marks:"))
+PercentageScore=Marks/1000*100  
+if(PercentageScore>=90):
+    print("Student has got A")
+elif(PercentageScore>=80 and PercentageScore<90):
+    print("Student has got B")
+elif(PercentageScore>=70 and PercentageScore<80):
+    print ("Student has got C")
+else :
+    print("Student has got D")
+print("Perecntage of student",PercentageScore)
